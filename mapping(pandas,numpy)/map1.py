78ea348b40130d3953a1f05e2d70f3a3911c9a89
Pratibha Rana <54569773+Pratibharana96@users.noreply.github.com>
2020-07-09 10:51:07 +0530

    Import text file in csv used pandas

--- mapping(pandas,numpy)/map1.py
+++ mapping(pandas,numpy)/map1.py
@@ -1,11 +1,18 @@
 import folium
-map=folium.Map(location=[29.898699, 79.576839],zoom_start=6,tiles='OpenStreetMap')
+import pandas as pd 
+
+data = pd.read_csv('Volcanoes.txt')
+# print(data)
+lat = list(data["LAT"])
+lon = list(data["LON"])
+# print(lon)
+map=folium.Map(location=[38.58, -99.09],zoom_start=6,tiles='OpenStreetMap')
 #2. create a feature group
 fg=folium.FeatureGroup(name="My Map")
 #3.Now We are adding multiple marker in our single map using for loop 
-for coordinates in([29.898699, 79.576839],[29.858699, 79.556839]):
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi,this is my home", tooltip=None, 
-    icon=folium.Icon(color='blue', icon_color='white', icon='info-sign', angle=0, prefix='glyphicon'), draggable=False))
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Hi,this is my home", tooltip=None, 
+    icon=folium.Icon(color='blue', icon_color='white', icon='info-sign', angle=0, prefix='glyphicon')))
     
     # fg.add_child(folium.Marker(location=[29.898699, 79.576839], popup="Hi I am Pratibha ", tooltip=None, 
     # icon=folium.Icon(color='blue', icon_color='white', icon='info-sign', angle=0, prefix='glyphicon'), draggable=False))
