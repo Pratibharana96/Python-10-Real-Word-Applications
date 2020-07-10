682a6bbcc483f128b8d021a57d57be43db21e818
Pratibha Rana <54569773+Pratibharana96@users.noreply.github.com>
2020-07-10 22:47:15 +0530

    Added elevation in the popup of multiple location

--- mapping(pandas,numpy)/map1.py
+++ mapping(pandas,numpy)/map1.py
@@ -5,13 +5,15 @@ data = pd.read_csv('Volcanoes.txt')
 # print(data)
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 # print(lon)
 map=folium.Map(location=[38.58, -99.09],zoom_start=6,tiles='OpenStreetMap')
 #2. create a feature group
 fg=folium.FeatureGroup(name="My Map")
 #3.Now We are adding multiple marker in our single map using for loop 
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="Hi,this is my home", tooltip=None, 
+for lt,ln,elv in zip(lat,lon,elev):
+    # print(type(elv))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(elv)+" m", tooltip=None, 
     icon=folium.Icon(color='blue', icon_color='white', icon='info-sign', angle=0, prefix='glyphicon')))
     
     # fg.add_child(folium.Marker(location=[29.898699, 79.576839], popup="Hi I am Pratibha ", tooltip=None, 
