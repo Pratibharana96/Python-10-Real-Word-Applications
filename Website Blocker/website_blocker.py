478d626f83b30af0bfbf21c72b08406047052cee
Pratibha Rana <54569773+Pratibharana96@users.noreply.github.com>
2020-07-22 09:05:01 +0530

    Check time,on that restrict opening  the web browser
    Here check the time after 4 pm it shows the Fun hour and changes the host's file by removing all the blocked website, Now all the websites are working in your browser.

--- Website Blocker/website_blocker.py
+++ Website Blocker/website_blocker.py
@@ -1,26 +1,29 @@
 import time
 from datetime import datetime as dt
+
 hosts_temp = "hosts"
-hosts_path = r"C:\Windows\System32\drivers\etc"
-redirects= "127.0.0.1"
-website_list= ["www.facebook.com","facebook.com","www.youtube.com"]
+hosts_path = r"hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
 
 while True:
-    
-
-    if dt(dt.now().year,dt.now().month,dt.now().day,6) < dt.now() <dt(dt.now().year,dt.now().month,dt.now().day,17):
-        print("Working  Hours...")
-        with open(hosts_temp,'r+') as hostsfile:
-            hostsdata= hostsfile.read()
-            for website in  website_list:
-            
-                if website in hostsdata:
+    if dt(dt.now().year,dt.now().month,dt.now().day,6) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
                     pass
                 else:
-                    hostsfile.write("\n"+ redirects +" "+ website+"\n")
-
-            
+                    file.write(redirect+" "+ website+"\n")
     else:
-        print("Fun Hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            # Problem of infinte writing all the lines to remove that we put the point at the top of first character
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
     time.sleep(5)
-   
